8e1fbd70da5a37bbeba8bae6debb4487a4638b9b
nezianyajc <jneziany@montgomerycollege.edu>
2021-06-20 17:43:00 -0400

    Code is running with switch statements

--- assignment_1/WiFiDiagnosis.java
+++ assignment_1/WiFiDiagnosis.java
@@ -24,6 +24,9 @@ public class WiFiDiagnosis {
             System.out.println("Please enter the number 1");
             issueNumber = s.nextInt();
             while(issueNumber != 0) {
+                if (issueNumber == 0) {
+                System.out.println(" You're done. The WiFi is working");
+                }
                 switch (issueNumber) {
                 case 0:
                 System.out.println(" You're done. The WiFi is working");
@@ -31,25 +34,53 @@ public class WiFiDiagnosis {
 
                 case 1: 
                 System.out.println("Reboot the computer and try again");
-                System.out.println("Did that fix the problem? If the problem is fixed enter 0 if not enter 2");
+                System.out.println("Did that fix the problem? If the problem is fixed enter 0 for yes if not enter 2 for no");
                 issueNumber = s.nextInt();
+                if (issueNumber == 0) {
+                System.out.println(" You're done. The WiFi is working");
+                }
                 break;
 
                 case 2: 
                 System.out.println("Reboot the router and try to connect");
                 System.out.println("Did that fix the problem? If the problem is fixed enter 0 if not enter 3");
                 issueNumber = s.nextInt();
+                if (issueNumber == 0) {
+                System.out.println(" You're done. The WiFi is working");
+                }
                 break;
 
                 case 3: 
-                System.out.println("Reboot the router and try to connect");
+                System.out.println("Make sure the cables connecting to the router are firmly plugged in ad power is getting to the router.");
                 System.out.println("Did that fix the problem? If the problem is fixed enter 0 if not enter 4");
                 issueNumber = s.nextInt();
+                if (issueNumber == 0) {
+                System.out.println(" You're done. The WiFi is working");
+                }
                 break;
+
+
+                case 4: 
+                System.out.println("Move the computer closer to the router and try to connect.");
+                System.out.println("Did that fix the problem? If the problem is fixed enter 0 if not enter 5");
+                issueNumber = s.nextInt();
+                if (issueNumber == 0) {
+                System.out.println(" You're done. The WiFi is working");
+                }
+                break;
+
+                
+                case 5: 
+                if (issueNumber == 5) {
+                System.out.println("Contact your ISP");
+                issueNumber = 0;
+                break;
+                }
+                break;
+                
                 }
 
             }
-               
 
         }
 
