33e262a1f74642731b7305cffbe2a5e3824cc0fe
nezianyajc <jneziany@montgomerycollege.edu>
2021-07-05 08:45:12 -0400

    Project 2 code compiles

--- chapter_7/projectTwo.java
+++ chapter_7/projectTwo.java
@@ -3,13 +3,21 @@ import java.util.ArrayList;
 public class projectTwo {
 
     public static void main(String[] args) {
+        String getArrayList;
         ArrayList<String> testList = new ArrayList<>();
         testList.add("Hello");
         testList.add("world");
         testList.add("goodbye");
+        getArrayList = f(testList);
+        System.out.println("The first character of each string " + getArrayList);
+
     }
 
-    public static void f() {
-
+    public static String f(ArrayList<String> testList) {
+        String hwg = "";
+        for (String test : testList) {
+            hwg = hwg + test.charAt(0);
+        }
+        return hwg;
     }
 }
