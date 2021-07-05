648251a24c2b6ce102d9ca8f6ebcb5b0867ffb75
nezianyajc <jneziany@montgomerycollege.edu>
2021-07-05 07:57:58 -0400

    Created initial setup

---
+++ chapter_7/projectTwo.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class projectTwo {
+
+    public static void main(String[] args) {
+        ArrayList<String> testList = new ArrayList<>();
+        testList.add("Hello");
+        testList.add("world");
+        testList.add("goodbye");
+    }
+
+    public static void f() {
+
+    }
+}
