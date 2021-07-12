a770463b35adebe982a368c24580cbb11bc4e67f
nezianyajc <jneziany@montgomerycollege.edu>
2021-07-12 16:16:35 -0400

    Completed Projects 1 & 2

--- chapter_8/Area.java
+++ chapter_8/Area.java
@@ -1,19 +1,15 @@
 public class Area {
 
-    private static double p = Math.PI;
-
     public static double getArea(double radius) {
-        return p * Math.pow((radius), 2);
+        return Math.PI * Math.pow((radius), 2);
     }
 
-    public static double getArea(int width, int length) {
-        double area;
-        area = length * width;
-        return area;
+    public static double getArea(double width, double length) {
+        return length * width;
     }
 
-    public static double getArea(double height, double radius) {
-        return p * Math.pow((radius), 2) * height;
+    public static double getArea(double pi, double height, double radius) {
+        return pi * radius * radius * height;
     }
 
 }
---
+++ chapter_8/LandTract.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class LandTract {
+    private int length;
+    private int width;
+
+    public int area() {
+        return length * width;
+    }
+
+    public boolean equals(LandTract land) {
+        if (length == land.length && width == land.width) {
+            return true;
+        } else if (length == land.width && width == land.length) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public String toString() {
+        return "LandTract with length" + length + "width" + width;
+    }
+
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        LandTract land1 = new LandTract();
+        LandTract land2 = new LandTract();
+
+        System.out.print("Enter length of first land tract:");
+        land1.length = obj.nextInt();
+        System.out.print("Enter width of first land tract:");
+        land1.width = obj.nextInt();
+        System.out.print("Enter length of Second land tract:");
+        land2.length = obj.nextInt();
+        System.out.print("Enter width of Second land tract:");
+        land2.width = obj.nextInt();
+        System.out.println(land1.toString() + " and area " + land1.area());
+        System.out.println(land2.toString() + " and area " + land2.area());
+        System.out.println(land1.equals(land2) ? "two tracks have same size" : "two tracks have different size");
+    }
+
+}
