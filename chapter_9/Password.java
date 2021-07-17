49b5e84190e66600b4bc4a0c48ef7e29d118dc21
nezianyajc <jneziany@montgomerycollege.edu>
2021-07-17 16:31:50 -0400

    Code runs

--- chapter_9/Password.java
+++ chapter_9/Password.java
@@ -3,24 +3,45 @@ public class Password {
     
     
     public static boolean authT(String password) {
-        boolean smurf;
+        boolean upCase, lowCase, dig;
+        upCase = false;
+        lowCase = false;
+        dig = false;                          
         if(password.length() >= 6) {
-            // char newPass = password.toCharArray();
-            for(int i = 0; i < password.length(); i++)
-                if(Character.isLetterOrDigit(password.charAt(i)) && Character.isUpperCase(password.charAt(i)) && Character.isLowerCase(password.charAt(i)) == true) {
+            for(int i = 0; i < password.length(); i++){
+                if(Character.isLetterOrDigit(password.charAt(i)) == true ){
+                    System.out.println("Condition met");
+                    dig = true;
+                } else {
+                    System.out.println("Invalid Password 1");
+                }
+
+                if(Character.isUpperCase(password.charAt(i)) == true ){
+                    System.out.println("Condition2 met");
+                    upCase = true;
+                } else {
+                    System.out.println("Invalid Password 2");
+                }
+
+                if(Character.isLowerCase(password.charAt(i)) == true ){
+                    System.out.println("Condition3 met");
+                    lowCase = true;
+                } else {
+                    System.out.println("Invalid Password 3");
+                }
+
+                if((dig && upCase && lowCase) == true){
                     System.out.println("Valid Password");
                     return true;
-                } else {
-                    System.out.println("Invalid Pass 2");
+                } else if((dig || upCase || lowCase) == false) {
+                    System.out.println("Invalid Password final pass");
                     return false;
                 }
+            }
         } else if(!(password.length() >= 6)) {
             System.out.println("Invalid Password");
-            return false;
         }
-
-        return false;
-    }   
-
+        return true;
+    }
 }
 
