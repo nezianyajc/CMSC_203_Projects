0d601361ea6eea48ff05a5148f40ce49ea5a5435
nezianyajc <jneziany@montgomerycollege.edu>
2021-07-25 15:40:18 -0400

    Finished Checking Account

--- CSMC_Lab6s/CheckingAccount.java
+++ CSMC_Lab6s/CheckingAccount.java
@@ -3,9 +3,19 @@ public class CheckingAccount extends BankAccount {
     private String name;
     private int initialAmount;
 
-    public CheckingAccount(String nam, int iniAmount) {
-        super();
-        this.name = nam;
-        this.initialAmount = iniAmount;
+    public CheckingAccount(String nam, int iniAmount, String acctNumber) {
+        super(nam, iniAmount);
+        acctNumber = super.getAccountNumber() + "-10";
     }
+
+    @Override
+    public boolean withdraw(double amount) {
+
+        double checkClearing = amount + FEE;
+        boolean flag;
+        flag = super.withdraw(checkClearing);
+        return flag;
+
+    }
+
 }
